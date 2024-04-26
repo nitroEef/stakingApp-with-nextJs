64aca87a7b9af32af1d1252f1fd759ae8583d077
nitroEef <mikebodetelli@gmail.com>
2024-04-26 12:26:49 +0100

    update

---
+++ src/Components/Anything.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Anything = () => {
+  return (
+    <div>
+      life mi don spoil
+    </div>
+  )
+}
+
+export default Anything
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Home from '@/Components/Home'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <h1 className='text-red-600 text-5xl'> hello world</h1>
+      <Home/>
     </div>
   )
 }
