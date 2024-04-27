b42a3e32711729e8ddcf5cea189f967aca2b2da5
nitroEef <mikebodetelli@gmail.com>
2024-04-27 13:49:53 +0100

    update

--- src/Components/displayPanel/DisplayPanel.jsx
+++ src/Components/displayPanel/DisplayPanel.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import StakedAmount from './StakedAmount'
+import RewardRate from './RewardRate'
+import EarnedReward from './EarnedReward'
 
 const DisplayPanel = () => {
   return (
-    <div className='flex justify-around mt-20'>
+   <div className='mt-20'>
+     <div className='flex justify-around'>
       <div className='text-4xl font-bold'>
         <h1>Connected Account</h1>
       </div>
 
       <div className=''>
         <h2 className='text-4xl font-bold'>Connected Account</h2>
-        <p className='text-center mt-3  text-2x1'>Unsupported</p>
+        <p className='text-center mt-3  text-2xl'>Unsupported</p>
       </div>
     </div>
+
+    <div className='flex justify-around px-40 mt-20'>
+<StakedAmount/>
+<RewardRate/>
+<EarnedReward/>
+    </div>
+   </div>
   )
 }
 
---
+++ src/Components/displayPanel/EarnedReward.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EarnedReward = () => {
+  return (
+    <div>
+       <div>
+      <h1 className='text-4xl font-bold'> Earned <br/> Reward:</h1>
+      <p className='text-center mt-3  text-2xl'>0</p>
+    </div>
+    </div>
+  )
+}
+
+export default EarnedReward
