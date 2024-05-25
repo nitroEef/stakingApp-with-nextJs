e104b07ec0a1c421ef50145e61ffefd4fa8b1517
nitroEef <mikebodetelli@gmail.com>
2024-05-25 13:32:37 +0100

    update

--- contracts/contracts/Staking.sol
+++ contracts/contracts/Staking.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-
 contract Staking is ReentrancyGuard {
     IERC20 public s_stakingToken;
     IERC20 public s_rewardToken;
@@ -14,18 +13,30 @@ contract Staking is ReentrancyGuard {
     uint public rewardPerTokenStored;
     uint public lastUpdateTime;
 
+    mapping(address=>uint) public stakedBalance;
+    mapping(address=>uint) public rewards;
+    mapping(address=>uint) public userRewardPerTokenPaid;
 
-    mapping (address=>uint) public stakedBalance;
-    mapping (address=>uint) public rewards;
-    mapping (address=>uint) public userRewardPerTokenPaid;
+    event Staked(address indexed user, uint256 indexed amount);
+    event Withdrawn(address indexed user, uint256 indexed amount);
+    event RewardsClaimed(address indexed user, uint256 indexed amount);
 
-    event Staked (address indexed user, uint256 indexed amount);
-    event Withdrawn (address indexed user, uint256 indexed amount);
-    event RewardsClaimed (address indexed user, uint256 indexed amount);
+    constructor(address stakingToken, address rewardToken) {
+        // IERC20 public s_stakingToken;
+        // IERC20 public s_rewardToken;
 
+        s_stakingToken=IERC20(stakingToken);
+        s_rewardToken=IERC20(rewardToken);        
+    }
+    function rewardPerToken() public view returns(uint) {
 
-    event Staked (adress indexed user, uint256 indexed amount);
+        if(totalStakedTokens == 0) {
+            return rewardPerTokenStored;
+        }
+        uint totalTime = block.timestamp - lastUpdateTime;
+        uint totalRewards = REWARD_RATE *totalTime;
+        return rewardPerTokenStored + totalRewards / totalStakedTokens;
 
-
+    }
 
 }
