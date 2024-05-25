0d37170d6a77c49725d32b0e78c59d45d6be602d
nitroEef <mikebodetelli@gmail.com>
2024-05-25 15:30:16 +0100

    update

---
+++ contracts/contracts/Staking.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract Staking is ReentrancyGuard {
+    IERC20 public s_stakingToken;
+    IERC20 public s_rewardToken;
+
+    uint public constant REWARD_RATE = 1e18;
+    uint private totalStakedTokens;
+    uint public rewardPerTokenStored;
+    uint public lastUpdateTime;
+
+    mapping(address => uint) public stakedBalance;
+    mapping(address => uint) public rewards;
+    mapping(address => uint) public userRewardPerTokenPaid;
+
+    event Staked(address indexed user, uint256 indexed amount);
+    event Withdrawn(address indexed user, uint256 indexed amount);
+    event RewardsClaimed(address indexed user, uint256 indexed amount);
+
+    constructor(address stakingToken, address rewardToken) {
+        // IERC20 public s_stakingToken;
+        // IERC20 public s_rewardToken;
+
+        s_stakingToken = IERC20(stakingToken);
+        s_rewardToken = IERC20(rewardToken);
+    }
+
+    function rewardPerToken() public view returns (uint) {
+        if (totalStakedTokens == 0) {
+            return rewardPerTokenStored;
+        }
+
+        uint totalTime = block.timestamp - lastUpdateTime;
+        uint totalRewards = REWARD_RATE * totalTime;
+
+        return rewardPerTokenStored + totalRewards / totalStakedTokens;
+    }
+
+    function earned(address account) public view returns (uint) {
+        return
+            stakedBalance[account] *
+            (rewardPerToken() - userRewardPerTokenPaid[account]);
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        _;
+    }
+
+    function stake(uint amount) external nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Amount must be greater than zero");
+        totalStakedTokens += amount;
+        stakedBalance[msg.sender] += amount;
+        emit Staked(msg.sender, amount);
+        bool success = s_stakingToken.transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        require(success, "Transfer failed");
+    }
+
+    function withdraw(
+        uint amount
+    ) external nonReentrant updateReward(msg.sender) {
+        uint reward = rewards[msg.sender];
+        require(reward > 0, "Amount must be greater than zero");
+        totalStakedTokens += amount;
+        stakedBalance[msg.sender] -= amount;
+        emit Withdrawn(msg.sender, amount);
+        bool success = s_stakingToken.transfer(msg.sender, amount);
+        require(success, "Transfer failed");
+    }
+
+    function getReward() external nonReentrant updateReward(msg.sender) {
+        uint reward = rewards[msg.sender];
+        require(reward > 0, "No rewared to claim");
+        rewards[msg.sender] = 0;
+        emit RewardsClaimed(msg.sender, reward);
+        bool success = s_rewardToken.transfer(msg.sender, reward);
+        require(success, "Transfer failed");
+    }
+}
